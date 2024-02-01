101d137fb67db28cbeb82bbe5355c0ef4d9ddb30
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-02-01 12:08:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React,{useState} from 'react';
+const allfruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
 import './../styles/App.css';
 
 const App = () => {
+  const [fruits, setFruits] = useState(allfruits)
+  const [search, setSearch] = useState('')
+  const handleSearch=(e)=>{
+    setFruits(fruits.filter(el=>el.toLowerCase().includes(e.target.value.toLowerCase())))
+    setSearch(e.target.value)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+    <input type="text" value={search} onChange={handleSearch} />
+      <ul>
+      {
+        fruits.map(el=><li>{el}</li>)
+      }
+      </ul>
     </div>
   )
 }
